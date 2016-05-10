c5e6793d5ecd7880f896cea31e42344b535f506f
carnellj <john.carnell@inin.com>
2016-05-10 08:45:08 -0400

    Got the config server and licensing server to talk with one another

--- chapter4-code/eurekasvr/src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
+++ chapter4-code/eurekasvr/src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
@@ -1,7 +1,9 @@
 package com.thoughtmechanix.eurekasvr;
 
+import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
