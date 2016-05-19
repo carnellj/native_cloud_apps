aa16c3672fb795d024bd68304a1c12bb7aec3891
carnellj <john.carnell@inin.com>
2016-05-19 16:48:42 -0400

    Actually committed the code

--- chapter4-code/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ chapter4-code/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @EnableAutoConfiguration
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableFeignClients
 @RefreshScope
 public class Application {
     public static void main(String[] args) {
--- chapter4-code/licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
+++ chapter4-code/licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -1,10 +1,19 @@
 package com.thoughtmechanix.licenses.services;
 
+import com.thoughtmechanix.licenses.clients.OrganizationClient;
 import com.thoughtmechanix.licenses.config.ServiceConfig;
 import com.thoughtmechanix.licenses.model.License;
+import com.thoughtmechanix.licenses.model.Organization;
 import com.thoughtmechanix.licenses.repository.LicenseRepository;
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,11 +24,49 @@ public class LicenseService {
     @Autowired
     private LicenseRepository licenseRepository;
 
+//    @Autowired
+//    private OrganizationClient orgClient;
+
     @Autowired
     ServiceConfig config;
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    private void getServiceJSON (String organizationId){
+        System.out.println("I am in discovery client");
+        ServiceInstance localInstance = discoveryClient.getLocalServiceInstance();
+        System.out.println( "Hello World: "+ localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort());
+
+        discoveryClient.getInstances("ORGANIZATIONSERVICE").forEach((ServiceInstance s) -> {
+            System.out.println("!!!!!" + s.getUri());
+        });
+    }
+
+
+    private Organization retrieveOrgInfo(String organizationId){
+        ResponseEntity<Organization> restExchange =
+                restTemplate.exchange(
+                        "http://ORGANIZATIONSERVICE/v1/organizations/{organizationId}",
+                        HttpMethod.GET,
+                        null, Organization.class, organizationId);
+
+        return restExchange.getBody();
+    }
+
     public License getLicense(String organizationId,String licenseId) {
         License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+
+       // Organization org = orgClient.getOrganization("organizationId");
+        getServiceJSON(organizationId);
+
+        Organization org = retrieveOrgInfo( organizationId);
+
+        System.out.println("!!!! Contact Name:  " + org.getContactName());
+        System.out.println("!!!! Contact Phone: " + org.getContactPhone());
         return license.withComment(config.getExampleProperty());
     }
 
