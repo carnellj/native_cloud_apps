4ebd0b365b10d788d8532e574286537ce6a71bea
carnellj <john.carnell@inin.com>
2016-06-07 05:45:04 -0400

    Committed the skeleton for chapter 5 code

---
+++ chapter5-code/confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.thoughtmechanix.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableAutoConfiguration
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
